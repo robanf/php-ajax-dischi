4410a0261aa8c7775bec9d6623f8a237a443e565
Roberto <robertoanfuso98@gmail.com>
2021-07-28 16:06:40 +0200

    first commit

---
+++ index-php.php
@@ -0,0 +1,114 @@
+<?php
+    $dischi=[
+        [
+            'title' => 'MI FAI IMPAZZIRE',
+            'author' => ' Sfera Ebbasta e Blanco',
+            'year' => 2021,
+            'poster' => 'https://www.honiro.it/wp-content/uploads/2021/06/BE426371-14C2-4BC1-98F6-8A999F95C1B5.jpeg'
+        ],
+        [
+            'title' => 'Every day',
+            'author' => 'Marshmello and Logic',
+            'year' => 2018,
+            'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2018/04/copertina-Everyday-logic-marshmello.jpg'
+        ],
+        [
+            'title' => 'In Radio',
+            'author' => 'Marracash',
+            'year' => 2015,
+            'poster' => 'https://s.mxmcdn.net/images-storage/albums5/1/0/0/3/4/1/35143001_350_350.jpg'
+        ],
+        [
+            'title' => 'Tranne te',
+            'author' => 'Fabri Fibra',
+            'year' => 2010,
+            'poster' => 'https://images.genius.com/1035e848755f68a3a2bedc35dcde11ee.564x564x1.jpg'
+        ],
+        [
+            'title' => '10 ragazze',
+            'author' => 'Rkomi',
+            'year' => 2021,
+            'poster' => 'https://images.genius.com/f5c3aee2ae8f91a0db5460354451aaf6.1000x1000x1.png'
+        ],
+        [
+            'title' => 'La mia signorina',
+            'author' => 'Neffa',
+            'year' => 2001,
+            'poster' => 'https://i.ytimg.com/vi/8q0fbDD7PlE/maxresdefault.jpg'
+        ],
+        [
+            'title' => 'Semaforo',
+            'author' => 'Willie Peyote',
+            'year' => 2019,
+            'poster' => 'https://m.media-amazon.com/images/I/71Q7QjZXSfL._SS500_.jpg'
+        ],
+        [
+            'title' => 'P.e.s',
+            'author' => 'Club Dogo',
+            'year' => 2012,
+            'poster' => 'https://i.scdn.co/image/ab67616d0000b2731a38b36b6bfba6ff02e198ef'
+        ],
+        [
+            'title' => 'SAD!',
+            'author' => 'XXXTentacion',
+            'year' => 2018,
+            'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2018/03/xxxtentacion-cover-album-2018.jpg'
+        ],
+        [
+            'title' => 'Save that shit',
+            'author' => 'Lil Peep',
+            'year' => 2017,
+            'poster' => 'https://i.ytimg.com/vi/V0r0CLhm23M/maxresdefault.jpg'
+        ],
+        [
+            'title' => 'Payphone',
+            'author' => ' Maroon 5',
+            'year' => 2012,
+            'poster' => 'https://i1.sndcdn.com/artworks-000028242476-8q2xtt-t500x500.jpg'
+        ],
+        [
+            'title' => 'River',
+            'author' => 'Eminem',
+            'year' => 2017,
+            'poster' => 'https://i.ytimg.com/vi/3BXDsVD6O10/maxresdefault.jpg'
+        ]
+    ]
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+        
+            <div class="container">
+
+                <div class="row">
+                    <?php foreach ($dischi as $key => $song) { ?>
+                        <div class="col-3 d-flex flex-column  align-items-center my-5">
+                            <img src="<?php echo $song['poster']; ?>" alt="" class="my-poster">
+                            <h3><?php echo $song['title']; ?></h3>
+                            <h4><?php echo $song['author']; ?></h4>
+                            <div><?php echo $song['year']; ?></div>
+                        </div>
+                        
+    
+                    <?php } ?> 
+
+
+                </div>
+
+            </div>
+
+            
+</body>
+</html>
